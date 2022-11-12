920037394d0341232e1e02f8b26e7ac0988c9d1a
Jhon <jhon@mail.com>
2022-11-12 21:44:07 +0300

    add script day 1

---
+++ js/search.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const search = () => {
+  const input = document.querySelector('.search-block > input'),
+        searchBtn = document.querySelector('.search-block > button');
+
+  
+
+  searchBtn.addEventListener('click', () => {
+    console.log(input.value);
+  });
+
+};
+
+search();
