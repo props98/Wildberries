249d72cbb6aaf9a808de540ccded00d573e25d60
Jhon <jhon@mail.com>
2022-11-13 16:01:43 +0300

    fix script

--- js/search.js
+++ js/search.js
@@ -4,8 +4,6 @@ const search = () => {
   const input = document.querySelector('.search-block > input'),
         searchBtn = document.querySelector('.search-block > button');
 
-  
-
   searchBtn.addEventListener('click', () => {
     console.log(input.value);
   });
